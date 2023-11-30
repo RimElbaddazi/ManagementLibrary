1f162fb6596ecb8f94beca069c9a0775f7716f17
Chaouki-FAHIM <Chaouki.Fahim@emsi-edu.ma>
2023-11-30 15:16:39 +0100

    Adding and Testing static config Micro-Servicefor routing book-service

---
+++ services/gateway-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,22 @@
+package org.sid.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r->r.path("/books/**").uri("http://localhost:8081/"))
+				.build();
+	}
+}
